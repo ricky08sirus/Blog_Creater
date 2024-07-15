384f01557785613c56e9e06aebf6361d1b0bd4f1
Aman <aman08jul@gmail.com>
2024-07-15 22:50:12 +0530

    add auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+//integrating the auth section
+import conf from "../conf/conf.js"
+
+import { Client, Account,ID} from "appwrite";
+
+export class AuthService {
+    client = new client();
+    account;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client);
+    }
+    async createAccount({email,password,name}){
+        try {
+            await this.account.create(ID.unique(),email,password,name)
+            if (userAccount) {
+                return this.login({email,password})
+                
+            } else {
+                return userAccount
+                
+            }            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+    //while building methods use try catch
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password)
+            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+            
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser()::",error);
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout()::",error);
+        }
+    }
+
+}
+
+const authService = new AuthService()
+
+export default authService
+// const client = new Client()
+//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');                 // Your project ID
+
+// const account = new Account(client);
