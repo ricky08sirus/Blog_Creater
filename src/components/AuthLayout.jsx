2fe8ac94e0abcf110b990bd6fda69a767df99022
Aman <aman08jul@gmail.com>
2024-07-26 22:31:32 +0530

    add authentication for routes

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Protected({children,authentication = true}) {
-  return (
-        <>{children}</>
-  )
+  const authStatus = useSelector((state)=> state.auth.status)
+  const navigate = useNavigate()
+  const [loader,setLoader] = useState(true)
+  useEffect(()=>{
+    if (authentication && authStatus!== authentication){
+      navigate("/login")
+    }else if(!authentication && authStatus!== authentication){
+      navigate("/")
+    }
+    setLoader(false)
+  },[authStatus,authentication,navigate])
+  return loader ? null : <>{children}</>
 }
 
 export default Protected
+
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,14 @@ import Login from "./pages/Login.jsx"
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import Signup from './pages/Signup.jsx'
+import allPosts from './pages/AllPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+
+
+
+
 const router = createBrowserRouter([
   {
     path:"/",
@@ -24,6 +32,42 @@ const router = createBrowserRouter([
         element: (
           <AuthLayout authentication ={false}>
             <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element:(
+          <AuthLayout authentication={false}>
+            <Signup/>
+
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/all-posts",
+        element:(
+          <AuthLayout authentication>
+            <allPosts/>   
+            {/* //we can check if the user is login or not by giving a query */}
+
+          </AuthLayout>
+        )
+
+      },
+      {
+        path:"/edit-post/:slug",
+        element:(
+          <AuthLayout authentication>
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path:"/post/:slug",
+        element:(
+          <AuthLayout authentication>
+            <Post/>
           </AuthLayout>
         )
       }
